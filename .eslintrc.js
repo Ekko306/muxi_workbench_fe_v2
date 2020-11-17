fc0a63c1c8f45ae1422f5c3037b57fdbec945231
Ekko306 <1186179435@qq.com>
2020-11-17 23:43:01 +0800

    Build the framework

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,34 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    parser:  '@typescript-eslint/parser', //定义ESLint的解析器
+    extends: [
+        'plugin:react/recommended' ,       //用于检测tsx文件
+        'plugin:@typescript-eslint/recommended'
+    ],//定义文件继承的子规范
+    plugins: ['@typescript-eslint'],//定义了该eslint文件所依赖的插件
+    env:{                          //指定代码的运行环境
+        browser: true,
+        node: true,
+    },
+    settings: {             //自动发现React的版本，从而进行规范react代码
+        "react": {
+            "pragma": "React",
+            "version": "detect"
+        }
+    },
+    parserOptions: {        //指定ESLint可以解析JSX语法
+        "ecmaVersion": 2019,
+        "sourceType": 'module',
+        "ecmaFeatures":{
+            jsx:true
+        }
+    },
     rules: {
-        // 禁止使用 var
-        'no-var': "error",
-        // 优先使用 interface 而不是 type
-        '@typescript-eslint/consistent-type-definitions': [
-            "error",
-            "interface"
-        ]
-    }
+    // 禁止使用 var
+    'no-var': "error",
+    // 优先使用 interface 而不是 type
+    '@typescript-eslint/consistent-type-definitions': [
+        "error",
+        "interface"
+    ]
+}
 }
---
+++ @types/react.d.ts
@@ -0,0 +1,8 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+        a: React.HTMLAttributes;
+        abbr: React.HTMLAttributes;
+        div: React.HTMLAttributes;
+        span: React.HTMLAttributes;
+    }
+}
